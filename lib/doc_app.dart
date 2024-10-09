d00af0388abb5e7fc90292cf251098231f81f3c7
Mohamed Ezz <madosharaf80@gmail.com>
2024-10-09 16:16:18 +0300

    added image splash. added native splash screen.

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_flutter/core/routing/app_router.dart';
 import 'package:flutter_complete_flutter/core/routing/routes.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,23 @@ import 'package:flutter_complete_flutter/doc_app.dart';
 
 void main() {
   // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-  //   statusBarColor: Colors.white,
-  // ));
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  //   DeviceOrientation.portraitDown,
-  // ]);
-  // SystemChrome.setEnabledSystemUIMode(
-  //   SystemUiMode.manual,
-  //   overlays: [SystemUiOverlay.top],
-  // );
-
+  // // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+  // //   statusBarColor: Colors.white,
+  // // ));
+  // // SystemChrome.setPreferredOrientations([
+  // //   DeviceOrientation.portraitUp,
+  // //   DeviceOrientation.portraitDown,
+  // // ]);
+  // // SystemChrome.setEnabledSystemUIMode(
+  // //   SystemUiMode.manual,
+  // //   overlays: [SystemUiOverlay.top],
+  // // );
+  // Future.delayed(const Duration(seconds: 3)).then((_) {
+  //   runApp(DocApp(
+  //     appRouter: AppRouter(),
+  //   ));
+  // });
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
 }
-
