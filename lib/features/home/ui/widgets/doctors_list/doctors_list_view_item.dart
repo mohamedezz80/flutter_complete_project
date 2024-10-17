c8f506db4476bf1ea7c3493b641ecb8c8e5dfc06
Mohamed Ezz <madosharaf80@gmail.com>
2024-10-17 16:28:53 +0300

    Enhanced doctors image shimmer loading.

--- lib/features/home/ui/widgets/doctors_list/doctors_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors_list/doctors_list_view_item.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/helpers/spacing.dart';
+import '../../../../../core/theming/colors.dart';
 import '../../../../../core/theming/styles.dart';
 import '../../../data/models/specialization_response_model.dart';
 
@@ -15,21 +18,46 @@ class DoctorsListViewItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 16.h),
       child: Row(
         children: [
-          // CachedNetworkImage(
-          //     imageUrl: "https://as2.ftcdn.net/v2/jpg/02/60/04/09/1000_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
-          //   progressIndicatorBuilder: (context, url, downloadProgress) =>
-          //   CircularProgressIndicator(value: downloadProgress.progress),
-          //   errorWidget: (context, url, error) => const Icon(Icons.error),
-          // ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              "https://as2.ftcdn.net/v2/jpg/02/60/04/09/1000_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
-              height: 120.h,
+          CachedNetworkImage(
+            imageUrl:
+            "https://as2.ftcdn.net/v2/jpg/02/60/04/09/1000_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorManager.lightGray,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
               width: 110.w,
-              fit: BoxFit.cover,
+              height: 120.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(12),
+          //   child: Image.network(
+          //     "https://as2.ftcdn.net/v2/jpg/02/60/04/09/1000_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+          //     height: 120.h,
+          //     width: 110.w,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
           horizontalSpace(16),
           Expanded(
             child: Column(
